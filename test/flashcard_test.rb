b1ab4871da9ad9f710bd8d3cc67e07cf5298cfce
katyjane8 <katy.jane8@gmail.com>
2017-09-24 13:35:44 -0600

    iteration1 complete with tests passing

--- test/flashcard_test.rb
+++ test/flashcard_test.rb
@@ -6,6 +6,23 @@ class CardTest < Minitest::Test
   def test_card_exists
     card = Card.new("What is the capital of Alaska", "Juneau")
 
-    assert_instance_of Card, card.question
+    assert_instance_of Card, card
+  end
+
+  def test_card_question
+    card = Card.new("What is the capital of Alaska", "Juneau")
+
+    assert_equal "What is the capital of Alaska", card.question
+  end
+
+  def test_card_answer
+    card = Card.new("What is the capital of Alaska", "Juneau")
+
+    assert_equal "Juneau", card.answer
+  end
+
+  def test_guess_from_card
+    card = Card.new("What is the capital of Alaska", "Juneau")
+    
   end
 end
